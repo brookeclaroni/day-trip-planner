931fd33ca9569ad7c02a578eff6740b3bfa1c6ad
Brooke Claroni <brookeclaroni@Brookes-MacBook-Pro.local>
2020-02-11 11:44:51 -0500

    front end of home screen done

--- DayTripPlanner/app/src/main/java/com/example/day_trip_planner/MainActivity.kt
+++ DayTripPlanner/app/src/main/java/com/example/day_trip_planner/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.day_trip_planner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner1: Spinner = findViewById(R.id.nearby_food_spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.cuisine_choices,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner1.adapter = adapter
+        }
+
+        val spinner2: Spinner = findViewById(R.id.attractions_spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.attraction_choices,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner2.adapter = adapter
+        }
     }
 }
