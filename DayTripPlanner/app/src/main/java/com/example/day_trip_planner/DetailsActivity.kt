1748dcd1c8af62376d3e4dc9a77fe0ab1fc16e93
Brooke Claroni <brookeclaroni@Brookes-MacBook-Pro.local>
2020-03-03 21:14:55 -0500

    handles having no results

--- DayTripPlanner/app/src/main/java/com/example/day_trip_planner/DetailsActivity.kt
+++ DayTripPlanner/app/src/main/java/com/example/day_trip_planner/DetailsActivity.kt
@@ -2,6 +2,7 @@ package com.example.day_trip_planner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,6 +20,11 @@ class DetailsActivity : AppCompatActivity() {
         val entries = getFakeEntries()
         val adapter = EntryAdapter(entries)
         recyclerView.adapter = adapter
+
+        if (entries.isEmpty())
+        {
+            Toast.makeText(this, "There are no results.", Toast.LENGTH_LONG).show()
+        }
     }
 
     fun getFakeEntries(): List<Entry> {
@@ -49,19 +55,19 @@ class DetailsActivity : AppCompatActivity() {
             ),
             Entry(
                 name = "Ford’s Theatre 4",
-                pricePoint = "$",
+                pricePoint = null,
                 rating = "1.25",
                 address = "511 10th St NW, Washington, DC 20004",
                 phone = null,
                 url = "https://www.yelp.com/biz/fords-theatre-washington?adjust_creative=keu-kOIeln4R7XPAEsPSYg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=keu-kOIeln4R7XPAEsPSYg"
             ),
             Entry(
-                name = "Ford’s Theatre 5",
-                pricePoint = null,
-                rating = "0.5",
-                address = "511 10th St NW, Washington, DC 20004",
-                phone = "+12023474833",
-                url = "https://www.yelp.com/biz/fords-theatre-washington?adjust_creative=keu-kOIeln4R7XPAEsPSYg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=keu-kOIeln4R7XPAEsPSYg"
+                name = "Wicked Waffle",
+                pricePoint = "$",
+                rating = "4",
+                address = "1712 I St NW, Washington, DC 20006",
+                phone = "+12029442700",
+                url = "https://www.yelp.com/biz/wicked-waffle-washington"
             )
         )
     }
